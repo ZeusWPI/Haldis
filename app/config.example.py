b3b1d25bb5572556f7b4da896eb2f9a9c61908fc
Feliciaan De Palmenaer <feliciaan@de-palmenaer.eu>
2015-03-26 21:59:51 +0100

    Removed some more unnecsarry settings

--- app/config.example.py
+++ app/config.example.py
@@ -6,7 +6,6 @@ class Configuration(object):
     DEBUG = True
     SECRET_KEY = '<change>'
     SLACK_WEBHOOK = '<add url>'
-    PROCESS = 'python test.py'
     LOGFILE = 'foodbot.log'
     ZEUS_KEY = '<fill in>'
     ZEUS_SECRET = '<fill in>'
