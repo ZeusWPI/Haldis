8c992beed715cfbb00593d21728fb16388db4f1d
Feliciaan De Palmenaer <feliciaan@de-palmenaer.eu>
2015-03-31 00:51:30 +0200

    new layout

--- app/app.py
+++ app/app.py
@@ -1,11 +1,14 @@
 from flask import Flask
-from flask.ext.bootstrap import Bootstrap
+from flask.ext.bootstrap import Bootstrap, StaticCDN
 from flask.ext.sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config.from_object('config.Configuration')
 Bootstrap(app)
+
+# use our own bootstrap theme
+app.extensions['bootstrap']['cdns']['bootstrap'] = StaticCDN()
 
 db = SQLAlchemy(app)
 
