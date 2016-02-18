6eeb7e8018ae1f5acbc4b26c69834815f2f38f6d
Feliciaan De Palmenaer <feliciaan@de-palmenaer.eu>
2016-02-18 11:26:41 +0100

    Remove prefixFix, no longer necessary

--- app/app.py
+++ app/app.py
@@ -19,22 +19,8 @@ db = SQLAlchemy(app)
 
 toolbar = DebugToolbarExtension(app)
 
-class PrefixFix(object):
-
-    def __init__(self, app, script_name):
-        self.app = app
-        self.script_name = script_name
-
-    def __call__(self, environ, start_response):
-        path = environ.get('SCRIPT_NAME', '') + environ.get('PATH_INFO', '')
-        environ['SCRIPT_NAME'] = self.script_name
-        environ['PATH_INFO'] = path[len(self.script_name):]
-        return self.app(environ, start_response)
-
 
 if not app.debug:
-    app.wsgi_app = PrefixFix(app.wsgi_app, '/haldis')
-
     timedFileHandler = TimedRotatingFileHandler(app.config['LOGFILE'], when='midnight', backupCount=100)
     timedFileHandler.setLevel(logging.DEBUG)
 
