ce0fe33c0ec7b3693bbc527cdbbb750c96b4d06f
Feliciaan De Palmenaer <feliciaan@de-palmenaer.eu>
2016-08-01 23:40:04 +0200

    Show prices for locations For @procrat

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,11 +1,11 @@
 __author__ = 'feliciaan'
 
-from flask import url_for, render_template, abort, redirect, request
+from flask import url_for, render_template, abort, redirect, request, abort
 from flask.ext.login import current_user, login_required
 from datetime import datetime, timedelta
 
 from app import app, db
-from models import Order, OrderItem
+from models import Order, OrderItem, Location
 
 # import views
 from views.order import get_orders
@@ -16,6 +16,19 @@ def home():
     recently_closed = get_orders(((Order.stoptime > prev_day) & (Order.stoptime < datetime.now())))
     return render_template('home.html', orders=get_orders(), recently_closed=recently_closed)
 
+
+@app.route('/location')
+def locations():
+    locs = Location.query.order_by('name')
+    return render_template('locations.html', locations=locs)
+
+
+@app.route('/location/<int:id>')
+def location(id):
+    loc = Location.query.filter(Location.id==id).first()
+    if loc is None:
+        abort(404)
+    return render_template('location.html', location=loc)
 
 @app.route('/about/')
 def about():
