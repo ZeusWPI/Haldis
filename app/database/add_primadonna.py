debca79f394716d1a32fa8dbf9250f04aeb79dad
Francis Begyn <francis.begyn@gmail.com>
2019-01-23 17:59:24 +0100

    Think this should work -- added primmadonna script

--- app/database/add_primadonna.py
+++ app/database/add_primadonna.py
@@ -6,40 +6,40 @@ def add():
     addAfhalen()
 
 pizzasTA = {
-        "Peperoni":750, 
-        "Basis pizza (extra garneringen zie site)":600, 
-        "Parma":750, 
-        "Margharita":600, 
-        "Funghi":715, 
-        "Mamma mia":715, 
-        "Napoletana":750, 
-        "Exotic":750, 
-        "Siciliana":750, 
-        "Michelangelo":750, 
-        "Roma":750, 
-        "Torno":750, 
-        "Bolognese":780, 
-        "Hawai":910, 
-        "Cipolla":910, 
-        "Dolce vita":910, 
-        "Valentino":910, 
-        "Vegateriana":1000, 
-        "La donna":1000, 
-        "Tropical":1000, 
-        "Quattro Stagioni":1000, 
-        "Romana":1000, 
-        "Diabolo":1000, 
-        "Turkish":1000, 
-        "Cesar":1000, 
-        "Calzone":1040, 
-        "Calzone Vegetariana":1040, 
-        "Quattro Formaggi":1040, 
-        "Frutti di mare":1040, 
-        "Gerookte ham en rucola":1040, 
-        "Van de chef":1170, 
-        "Milano":1170, 
-        "Soronto":1260, 
-        "Primma Donna":1260, 
+        "Peperoni":750,
+        "Basis pizza (extra garneringen zie site)":600,
+        "Parma":750,
+        "Margharita":600,
+        "Funghi":715,
+        "Mamma mia":715,
+        "Napoletana":750,
+        "Exotic":750,
+        "Siciliana":750,
+        "Michelangelo":750,
+        "Roma":750,
+        "Torno":750,
+        "Bolognese":780,
+        "Hawai":910,
+        "Cipolla":910,
+        "Dolce vita":910,
+        "Valentino":910,
+        "Vegateriana":1000,
+        "La donna":1000,
+        "Tropical":1000,
+        "Quattro Stagioni":1000,
+        "Romana":1000,
+        "Diabolo":1000,
+        "Turkish":1000,
+        "Cesar":1000,
+        "Calzone":1040,
+        "Calzone Vegetariana":1040,
+        "Quattro Formaggi":1040,
+        "Frutti di mare":1040,
+        "Gerookte ham en rucola":1040,
+        "Van de chef":1170,
+        "Milano":1170,
+        "Soronto":1260,
+        "Primma Donna":1260,
         "Pasta (zie site voor opties)":900
         }
 
@@ -48,9 +48,9 @@ def addTA():
     primadonna_takeaway.configure("Primadonna (takeaway laten bezorgen)", "Overpoortstraat 46 9000 Gent, tel: 0475 40 13 00", "https://www.takeaway.com/be-en/prima-donna")
     db.session.add(primadonna_takeaway)
 
-    for pizza in pizzaTA.keys:
+    for pizza, price in pizzasTA.items():
         entry = Product()
-        entry.configure(primadonna_takeaway, pizza, pizzasTA.get(pizza, 0))
+        entry.configure(primadonna_takeaway, pizza, price)
         db.session.add(entry)
 
 pizzasAfhalen = {
@@ -96,8 +96,8 @@ def addAfhalen():
     primadonna_afhalen.configure("Primadonna (bellen en afhalen)", "Overpoortstraat 46 9000 Gent, tel: 0475 40 13 00", "http://primadonnagent.be/Menu.html")
     db.session.add(primadonna_afhalen)
 
-    for pizza in pizzaAfhalen.keys:
+    for pizza, price in pizzasAfhalen.items():
         entry = Product()
-        entry.configure(primadonna_afhalen, pizza, pizzasAfhalen.get(pizza, 0))
+        entry.configure(primadonna_afhalen, pizza, price)
         db.session.add(entry)
 
