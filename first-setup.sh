c249bdcbd4208890fd5f89a546e247487c2e860d
mcbloch <alazeo@live.be>
2019-04-01 23:42:59 +0200

    add venv creation to script, add to readme and gitignore

--- first-setup.sh
+++ first-setup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # A simple file to run all instructions from the README
 ## this should be run in the root of the repository
-pip3 install -r requirements.txt
+
+if [ ! -d "vent" ]; then
+    python -m venv venv
+fi
+
+venv/bin/pip install -r requirements.txt
 cd app
 cp config.example.py config.py
 cp -t . database/*
-python3 create_database.py
+venv/bin/python create_database.py
 rm -f add_* create_database.py
-python3 haldis.py runserver
+venv/bin/python haldis.py runserver
 cd ..
