07895c5e8799ab36ebd14c1f3610eec38177c17c
mcbloch <alazeo@live.be>
2019-04-05 01:48:59 +0200

    add some nice formatting

--- first-setup.sh
+++ first-setup.sh
@@ -2,24 +2,31 @@
 # A simple file to run all instructions from the README
 ## this should be run in the root of the repository
 
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+B="\n${bold}"
+E="${normal}"
+
 if [ ! -d "venv" ]; then
-    echo "No venv found, creating a new one"
+    echo -e "${B} No venv found, creating a new one ${E}"
     python -m venv venv
 fi
 source venv/bin/activate
 
-echo "Installing pip-tools"
+
+echo -e "${B} Installing pip-tools ${E}"
 pip install pip-tools
 
-echo "Downloading dependencies"
+echo -e "${B} Downloading dependencies ${E}"
 pip-sync
 
-echo "Copying config template. All custom config options can be set in the config.py file"
+echo -e "${B} Copying config template. All custom config options can be set in the config.py file ${E}"
 cd app
 cp config.example.py config.py
 cd ..
 
-echo "Seeding database"
+echo -e "${B} Seeding database ${E}"
 ./populate-db.sh
 
-echo "You can now run the server with 'python app/haldis.py runserver'"
+echo -e "${B} Activate your venv using 'source venv/bin/activate'.\nThen run the server with 'python app/haldis.py runserver' ${E}"
