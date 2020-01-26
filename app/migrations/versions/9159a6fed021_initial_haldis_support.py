4b99eff7c852be0faf2cd377c4f2883bfc05c5bc
Midgard <midgard@zeus.ugent.be>
2020-01-26 19:39:21 +0100

    Put migration table in migration to avoid import troubles

--- app/migrations/versions/9159a6fed021_initial_haldis_support.py
+++ app/migrations/versions/9159a6fed021_initial_haldis_support.py
@@ -17,7 +17,39 @@ import sqlalchemy as sa
 from sqlalchemy.sql import table, column, text
 
 from hlds.definitions import location_definitions
-from data.migration import LOCATION_LEGACY_TO_HLDS, DISH_LEGACY_TO_HLDS
+
+LOCATION_LEGACY_TO_HLDS = {
+    2: "blauw_kotje",
+    3: "ocean_garden",
+    4: "delhaize",
+    5: "simpizza",
+    6: "shell_tankstation",
+    7: "pitta_pinte",
+    8: "lan_pizza",
+    9: "oriental",
+    10: "gouden_sate",
+    11: "quick",
+    12: "dominos",
+    13: "stella_di_mare",
+    14: "olifantje",
+    15: "pauls_boutique",
+    16: "fritoloog",
+    17: "andromeda",
+    19: "pita_cece",
+    20: "pita_uniq",
+    21: "basic_italian",
+    22: "pizza_hut",
+    23: "test",
+    25: "primadonna_takeaway",
+    26: "primadonna_delivery",
+    28: "s5",
+    29: "fitchen",
+    30: "kebab_anadolu",
+}
+
+DISH_LEGACY_TO_HLDS = {
+    # TODO
+}
 
 
 def upgrade():
