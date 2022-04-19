ba1b37f5fe57611a95acc1b2c39ff300c16aff3b
Jan-Pieter Baert <jan-pieter@baert.jp.net>
2022-04-19 20:40:06 +0200

    Update populate-db to be less hacky

--- populate-db.sh
+++ populate-db.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 cd "$(dirname "$0")/app"
 cp database/* .
-../venv/bin/python create_database.py setup_database
+env python create_database.py setup_database
 rm -f add_* create_database.py muhscheme.txt
