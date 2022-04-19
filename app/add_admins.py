492d1ca91cc95c5f1fd86fbc88df5286bb95db71
Jan-Pieter Baert <jan-pieter@baert.jp.net>
2022-04-19 20:08:29 +0200

    Update script to read admins from configuration file

--- app/add_admins.py
+++ app/add_admins.py
@@ -2,24 +2,13 @@
 from models import User
 
 from app import db
+from models import User
+from config import Configuration
 
 
 def add() -> None:
     """Add users as admin."""
-    feli = User()
-    feli.configure("feliciaan", True, 0)
-    db.session.add(feli)
-
-    destro = User()
-    destro.configure("destro", True, 0)
-    db.session.add(destro)
-
-    iepoev = User()
-    iepoev.configure("iepoev", True, 1)
-    db.session.add(iepoev)
-
-    flynn = User()
-    flynn.configure("flynn", True, 0)
-    db.session.add(flynn)
-
-    # To future developers, add yourself here
+    for username in Configuration.HALDIS_ADMINS:
+        user = User()
+        user.configure(username, True, 0)
+        db.session.add(user)
